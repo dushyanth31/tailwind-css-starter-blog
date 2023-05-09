c1e5ad009a778aa53789eeae9a1b4d5d2ca71b8c
DanUsh41 <86404304+dushyanth31@users.noreply.github.com>
2023-05-09 23:05:35 +0530

    initial commit

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,8 +1,8 @@
 export default function PageTitle({ children }) {
   return (
     <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+    <p class="text-gray-600">{{ 7 }} min read</p>
       {children}
-<p class="text-gray-600">{{ 7 }} min read</p>
     </h1>
   )
 }
