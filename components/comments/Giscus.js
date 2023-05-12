060999c5949b11e200a23162b77420d03a77bf50
DanUsh41 <86404304+dushyanth31@users.noreply.github.com>
2023-05-12 22:32:17 +0530

    initial commit

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -32,16 +32,16 @@ const Giscus = () => {
 
     const script = document.createElement('script')
     script.src = 'https://giscus.app/client.js'
-    script.setAttribute('data-repo', "dushyanth31/tailwind-css-starter-blog")
-    script.setAttribute('data-repo-id', "R_kgDOJJjdyA")
-    script.setAttribute('data-category', "General")
-    script.setAttribute('data-category-id', "DIC_kwDOJJjdyM4CWcnX")
-    script.setAttribute('data-mapping', "pathname")
-    script.setAttribute('data-reactions-enabled', "1")
-    script.setAttribute('data-emit-metadata', "0")
-    script.setAttribute('data-input-position', "bottom")
+    script.setAttribute('data-repo', 'dushyanth31/tailwind-css-starter-blog')
+    script.setAttribute('data-repo-id', 'R_kgDOJJjdyA')
+    script.setAttribute('data-category', 'General')
+    script.setAttribute('data-category-id', 'DIC_kwDOJJjdyM4CWcnX")
+    script.setAttribute('data-mapping', 'pathname')
+    script.setAttribute('data-reactions-enabled', '1')
+    script.setAttribute('data-emit-metadata', '0')
+    script.setAttribute('data-input-position', 'bottom')
     script.setAttribute('data-lang', lang)
-    script.setAttribute('data-theme', "preferred_color_scheme")
+    script.setAttribute('data-theme', 'preferred_color_scheme')
     script.setAttribute('crossorigin', 'anonymous')
     script.async = true
 
