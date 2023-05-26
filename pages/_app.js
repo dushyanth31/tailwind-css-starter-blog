4d71069bccd768c08256f845ca515216973a65ec
DanUsh41 <86404304+dushyanth31@users.noreply.github.com>
2023-05-26 22:45:24 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,30 @@
-import '@/css/tailwind.css'
-import '@/css/prism.css'
-import 'katex/dist/katex.css'
-
-import '@fontsource/inter/variable-full.css'
-
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import { Analytics } from '@vercel/analytics/react'
 
-const isDevelopment = process.env.NODE_ENV === 'development'
-const isSocket = process.env.SOCKET
-
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+    <>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </ThemeProvider>
+      <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+        {typeof window !== 'undefined' && process.env.NODE_ENV === 'development' && process.env.SOCKET && (
+          <ClientReload />
+        )}
+        <Analytics />
+        <div>
+          <LayoutWrapper>
+            <Component {...pageProps} />
+          </LayoutWrapper>
+        </div>
+      </ThemeProvider>
+    </>
   )
 }
+
+export default MyApp
